3133389d86c175ff04a515ccdcfd1b6b381b5d6d
chepchanseha <chepchanseyha@gmail.com>
2018-08-14 00:29:18 +0700

    the last one

--- login/login.js
+++ login/login.js
@@ -2,24 +2,23 @@ function pasuser() {
 	event.preventDefault();
 	var form = document.forms["login"];
 	if (form.id.value=="seyha") { 
-	if (form.pass.value=="seyha") {
-		window.location.href ="../homestudent/Home.html"
-	}
-	else {
-		alert("Invalid Password")
-	}
+		if (form.pass.value=="seyha") {
+			window.location.href ="homestudent/Home.html"
+		}
+		else {
+			alert("Invalid Password")
+		}
 	}
 	else if (form.id.value=="sarakorn") { 
-	if (form.pass.value=="sarakorn") {
-		window.location.href ="../hometeacher/Home.html"
-	}
-	else {
-		alert("Invalid Password")
-	}
+		if (form.pass.value=="sarakorn") {
+			window.location.href ="hometeacher/Home.html"
+		}
+		else {
+			alert("Invalid Password")
+		}
 	}
 	else {
 		alert("Invalid UserID")
-	
-}
+	}
 }
 
