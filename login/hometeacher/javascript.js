7fa474f2084db00142db2a5aaa56d61eee9b3704
chepchanseha <chepchanseyha@gmail.com>
2018-08-21 13:52:38 +0700

    d

--- login/hometeacher/javascript.js
+++ login/hometeacher/javascript.js
@@ -28,13 +28,3 @@ $(function() {
 
 
 /* pop up */
- $(document).ready(function () {
-     $("button").click(function () {
-         $(".pop").fadeIn(300);
-         positionPopup();
-     });
-
-     $(".pop > span, .pop").click(function () {
-         $(".pop").fadeOut(300);
-     });
- });
