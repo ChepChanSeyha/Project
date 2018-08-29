d34ce9890a2fd3c6879fe544bc1c2752acf843db
chepchanseha <chepchanseyha@gmail.com>
2018-08-29 14:33:21 +0700

    khos syntax js

--- login/login.js
+++ login/login.js
@@ -1,24 +1,23 @@
 function pasuser() {
 	event.preventDefault();
-	var form = document.forms["login"];
+	var form = document.forms[login];
 	if (form.id.value=="seyha") { 
 		if (form.pass.value=="seyha") {
-			window.location.href ="homestudent/Home.html"
+			window.location.href ="homestudent/Home.html";
 		}
 		else {
-			alert("Invalid Password")
+			alert("Invalid Password");
 		}
 	}
 	else if (form.id.value=="sarakorn") { 
 		if (form.pass.value=="sarakorn") {
-			window.location.href ="hometeacher/Home.html"
+			window.location.href ="hometeacher/Home.html";
 		}
 		else {
-			alert("Invalid Password")
+			alert("Invalid Password");
 		}
 	}
 	else {
-		alert("Invalid UserID")
+		alert("Invalid UserID");
 	}
 }
-
